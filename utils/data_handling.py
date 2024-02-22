ec2275d066b132879564e1c2e1af95f9e97e2b3c
Paul VanKoughnett <allispaul@gmail.com>
2024-02-21 22:32:24 -0600

    Add n_items argument to SpectrogramDataset, allowing for the construction of datasets with fewer items.

--- utils/data_handling.py
+++ utils/data_handling.py
@@ -80,12 +80,26 @@ class SpectrogramDataset(torch.utils.data.Dataset):
     Args:
       metadata_df: DataFrame containing spectrogram offsets, paths to .npy
         files, and vote labels.
+      n_items (optional): If not None, generate a dataset with this many items
+        by selecting a random subset of metadata_df.
       item_transforms (optional): Transforms to be applied to each item before
         returning it.
+      preloaded (default False): whether to load every spectrogram into memory at
+        initialization. I estimate that this uses 5-6 GB of RAM and is about
+        15% faster during training and 10% faster during inference.
     """
-    def __init__(self, metadata_df, item_transforms=None):
-        self.metadata_df = metadata_df
+    def __init__(self, metadata_df, n_items=None, item_transforms=None,
+                 preloaded=False):
+        if n_items is not None:
+            self.metadata_df = metadata_df.sample(n=n_items).copy()
+        else:
+            self.metadata_df = metadata_df
         self.item_transforms = item_transforms
+        if preloaded:
+            self.spec_dict = {npy_path: torch.from_numpy(np.load(npy_path))
+                              for npy_path in self.metadata_df.spec_npy_path.unique()}
+        else:
+            self.spec_dict = None
         
     def __len__(self):
         return len(self.metadata_df)
@@ -93,7 +107,10 @@ class SpectrogramDataset(torch.utils.data.Dataset):
     def __getitem__(self, i):
         npy_path = self.metadata_df["spec_npy_path"].iloc[i]
         offset = int(self.metadata_df["spectrogram_label_offset_seconds"].iloc[i])
-        tens = torch.from_numpy(np.load(npy_path))[:, offset//2:offset//2+300]
+        if self.spec_dict is not None:
+            tens = self.spec_dict[npy_path][:, offset//2:offset//2+300]
+        else:
+            tens = torch.from_numpy(np.load(npy_path))[:, offset//2:offset//2+300]
         if self.item_transforms is not None:
             tens = self.item_transforms(tens)
         expert_votes = self.metadata_df[[
@@ -110,42 +127,3 @@ class SpectrogramTestDataset(SpectrogramDataset):
         tens = torch.from_numpy(np.load(npy_path))
         target = None
         return tens, target
-        
-class SpectrogramDatasetPreloaded(torch.utils.data.Dataset):
-    """Dataset with only spectrogram data. Unlike SpectrogramDataset, this
-    loads every spectrogram into memory at initialization. I estimate that this
-    uses 5-6 GB of RAM and is about 15% faster during training and 10% faster
-    during inference.
-    
-    Args:
-      metadata_df: DataFrame containing spectrogram offsets, paths to .npy
-        files, and vote labels.
-      item_transforms (optional): Transforms to be applied to each item before
-        returning it.
-    """
-    def __init__(self, metadata_df, item_transforms=None):
-        self.metadata_df = metadata_df
-        self.item_transforms = item_transforms
-        # load all data
-        self.spec_dict = {npy_path: torch.from_numpy(np.load(npy_path))
-                          for npy_path in metadata_df.spec_npy_path.unique()}
-        
-    def __len__(self):
-        return len(self.metadata_df)
-    
-    def __getitem__(self, i):
-        npy_path = self.metadata_df["spec_npy_path"].iloc[i]
-        offset = int(self.metadata_df["spectrogram_label_offset_seconds"].iloc[i])
-        tens = self.spec_dict[npy_path][:, offset//2:offset//2+300]
-        if self.item_transforms is not None:
-            tens = self.item_transforms(tens)
-        expert_votes = self.metadata_df[[
-            "seizure_vote", "lpd_vote", "gpd_vote",
-            "lrda_vote", "grda_vote", "other_vote"
-        ]].iloc[i]
-        # target should be float so that nn.KLDivLoss works
-        target = torch.tensor(np.asarray(expert_votes)).float()
-        return tens, target
-        
-    
-        
